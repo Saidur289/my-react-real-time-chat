ee587669868b6e167f470775f4bb0937a98de5a1
Saidur289 <saidur.riaz1@gmailcomm>
2025-04-22 19:05:33 +0600

    finish create chat header componnets task on chat page

--- client/src/pages/Chat/components/chat-container/components/chat-header/ChatHeader.jsx
+++ client/src/pages/Chat/components/chat-container/components/chat-header/ChatHeader.jsx
@@ -1,15 +1,49 @@
 import { RiCloseFill } from "react-icons/ri";
 import { useAppStore } from "../../../../../../store";
+import { Avatar, AvatarImage } from "../../../../../../components/ui/avatar";
+import { HOST } from "../../../../../../utils/constaints";
+import { getColor } from "../../../../../../lib/utils";
 
 const ChatHeader = () => {
+    // data is coming from new dm page after selection 
   const { closeChat, selectedChatData, selectedChatType } = useAppStore();
-  console.log(selectedChatData, selectedChatType);
+//   console.log(selectedChatData, selectedChatType);
   return (
     <div className="h-[10vh] border-b-2 border-[#2f303b] flex items-center justify-center">
-      <div className="flex gap-5 items-center">
-        {/* first div */}
+      <div className="flex gap-5 items-center w-full justify-between">
+        {/* first   div  second parent */}
 
-        <div className="flex gap-3 items-center justify-center"></div>
+        <div className="flex gap-3 items-center justify-center">
+              {/* first child  */}
+              <div className="w-12 h-12 relative">
+              
+          <Avatar className="h-12 w-12 rounded-full overflow-hidden">
+            {selectedChatData?.image ? (
+              <AvatarImage
+                src={`${HOST}/${selectedChatData?.image}`}
+                alt="profile image"
+                className="object-cover w-full h-full bg-black"
+              />
+            ) : (
+              <div
+                className={`uppercase rounded-full h-12 w-12 text-lg border-[1px] flex items-center justify-center  ${getColor(
+                  selectedChatData?.color
+                )}`}
+              >
+                {selectedChatData?.firstName
+                  ? selectedChatData?.firstName.split("").shift()
+                  : selectedChatData.email.split("").shift()}
+              </div>
+            )}
+          </Avatar>
+              </div> 
+              {/* second child */}
+              <div>
+                {
+                    selectedChatType === 'contact' && selectedChatData?.firstName ? `${selectedChatData?.firstName} ${selectedChatData?.lastName}`: selectedChatData?.email
+                }
+              </div>
+        </div>
         {/* second div */}
         <div className="flex items-center justify-center gap-5">
           {/* first item */}
