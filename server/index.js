5d62403e522c9319f462694c80f9d56143943b80
Saidur289 <saidur.riaz1@gmailcomm>
2025-04-26 20:21:45 +0600

    small changes in index js server side for deploy

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ app.use('/api/channel', channelRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("*", (req, res) => {
+  app.all('/{*any}', (req, res) => {
     res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
   });
 }
