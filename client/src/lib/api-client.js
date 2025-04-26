5bce8be70312a4d9d73ec2551e1365899c4aeb21
Saidur289 <saidur.riaz1@gmailcomm>
2025-04-26 18:03:28 +0600

    do some function

--- client/src/lib/api-client.js
+++ client/src/lib/api-client.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 import { HOST } from '../utils/constaints'
 
+
+
+
+
 export const apiClient = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? HOST : "",
-})
+    baseURL: HOST,
+    
+  });
