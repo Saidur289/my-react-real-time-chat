6888b54f4d0cf593e94691866664065e458f56b6
Saidur289 <saidur.riaz1@gmailcomm>
2025-04-26 18:03:56 +0600

    do some task

--- client/src/utils/constaints.js
+++ client/src/utils/constaints.js
@@ -1,5 +1,8 @@
 // auth route start here
-export const HOST = "http://localhost:8747";
+export const HOST =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:8747"
+    : "https://my-react-real-time-chat-1.onrender.com";
 export const AUTH_ROUTES = "/api/auth";
 export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
 export const LOGIN_ROUTE = `${AUTH_ROUTES}/login`;
